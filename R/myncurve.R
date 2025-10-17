ec484c5772a168e2b6e462cbf5d992f25fcfbbaf
smithspy01 <smithspy01@gmail.com>
2025-10-17 18:02:12 -0500

    added example to myncurve

--- R/myncurve.R
+++ R/myncurve.R
@@ -8,6 +8,8 @@
 #' @export
 #'
 #' @examples
+#' myncurve(mu=10,sigma=5, a=6)
+
 myncurve <- function(mu, sigma, a) {
   curve(dnorm(x, mean = mu, sd = sigma),
         xlim = c(mu - 3 * sigma, mu + 3 * sigma),
