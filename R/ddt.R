fcd90c1d53a8748433007f3e6e0b88345c304c0a
smithspy01 <smithspy01@gmail.com>
2025-10-24 10:16:25 -0500

    DDT data set and documentation FIREDAM data set and documentation

---
+++ R/ddt.R
@@ -0,0 +1,14 @@
+#' DDT Concentration Data
+#'
+#' This dataset contains measurements related to DDT concentrations in fish species sampled from various rivers.
+#'
+#' @format A data frame with 145 rows and 6 columns:
+#' \describe{
+#'   \item{SPECIES}{Fish species}
+#'   \item{RIVER}{Name of the river}
+#'   \item{DDT}{Concentration of DDT [ppm]}
+#'   \item{LENGTH}{Length of the fish [cm]}
+#'   \item{WEIGHT}{Weight of the fish [g]}
+#' }
+#' @source Textbook example dataset from MATH 4753
+"ddt"
---
+++ R/fire.R
@@ -0,0 +1,11 @@
+#' FIre damage data
+#'
+#' Dataset with fire damage statistics
+#'
+#' @format A data frame with 15 rows and 2 columns:
+#' \describe{
+#'   \item{DISTANCE}{Distance the fire burnedd}
+#'   \item{DAMAGE}{The amount of damage the fire caused}
+#' }
+#' @source Textbook example dataset from MATH 4753
+"fire"
